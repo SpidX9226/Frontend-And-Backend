7f65aa4003d377e89e0d5cebe100391584841801
Said <saidmahov10@gmail.com>
2026-05-10 16:07:14 +0300

    docs: add openAPI + swagger docs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,21 @@
 import express from "express";
-import type {
-  Request,
-  Response,
-  NextFunction
-} from "express";
+import type { Request, Response, NextFunction } from "express";
 import cors from "cors";
-import productsRouter from './routes/products.js'
-import logger from './middleware/logger.js'
-import { error } from "node:console";
-
+import productsRouter from './routes/products.js';
+import logger from './middleware/logger.js';
+import SwaggerUi from 'swagger-ui-express';
+import { parse } from 'yaml';
+import { readFileSync } from "node:fs";
+import path from "node:path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const yamlFile = readFileSync(
+    path.join(process.cwd(), "src", "docs", "api-docs.yaml"),
+    "utf-8"
+);
+const swaggerSpec = parse(yamlFile);
 
 app.use(
     cors({
@@ -21,6 +25,8 @@ app.use(
 
 app.use(express.json());
 app.use(logger);
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerSpec));
 
 app.get('/', (req, res) => {
     res.send("Express API is running. Try  /api/products");
@@ -56,4 +62,5 @@ app.use((req: Request, res: Response) => {
 
 app.listen(PORT, () => {
     console.log(`Server started: http://localhost:${PORT}`);
+    console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
 });
